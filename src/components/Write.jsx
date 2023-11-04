5afdd417c9ea0da8caed0499bf76120d8d715f44
Aamish Hussain <aamishhussain23@gmail.com>
2023-11-05 00:03:43 +0530

    pc and phone responsive

--- src/components/Write.jsx
+++ src/components/Write.jsx
@@ -4,15 +4,16 @@ import Note from './Note';
 import sendimg from '../assets/sendimg.png';
 import backimg from '../assets/backimg.png';
 import Text from './Text';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Write = ({ users }) => {
+  const navigate = useNavigate()
   const { noteId } = useParams();
   const [userName, setUserName] = useState('');
   const [userColor, setUserColor] = useState('');
   const [shortname, setShortname] = useState('');
   const [newText, setNewText] = useState('');
-  const [myTexts, setMyTexts] = useState([]); // Initialize as an empty array
+  const [myTexts, setMyTexts] = useState([]); 
 
   useEffect(() => {
     console.log('useEffect run');
@@ -21,7 +22,7 @@ const Write = ({ users }) => {
       setUserName(user.userName);
       setUserColor(user.userColor);
       setShortname(user.shortName);
-      setMyTexts(user.texts ? Object.values(user.texts) : []); // Set the user's texts
+      setMyTexts(user.texts ? Object.values(user.texts) : []); 
     }
   }, [noteId, users]);
 
@@ -31,7 +32,7 @@ const Write = ({ users }) => {
 
   const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
-      e.preventDefault(); // Prevent the default behavior (creating a new line)
+      e.preventDefault(); 
       handleSubmit();
     }
   }
@@ -50,13 +51,11 @@ const Write = ({ users }) => {
       if (user.id === parseInt(noteId)) {
         user.texts = user.texts || {};
 
-        // Increment the textCount and timeCount
         user.textCount = (user.textCount || 0) + 1;
 
-        // Create a new text entry object
         user.texts[`text${user.textCount}`] = {
           data: newText,
-          date: formatDate(new Date()), // Format the date
+          date: formatDate(new Date()),
           time: new Date().toLocaleTimeString(),
         };
 
@@ -65,26 +64,27 @@ const Write = ({ users }) => {
       return user;
     });
 
-    // Update the component state to reflect the changes
     setMyTexts(Object.values(updatedUsers.find((user) => user.id === parseInt(noteId)).texts || {}));
 
-    // Update localStorage with the updated array while keeping existing data intact
     const updatedArray = users.map((user) => {
       if (user.id === parseInt(noteId)) {
         return updatedUsers.find((u) => u.id === user.id);
       }
       return user;
     });
-    localStorage.setItem('users', JSON.stringify(updatedArray));
 
-    // Clear the textarea
+    localStorage.setItem('users', JSON.stringify(updatedArray));
     setNewText('');
+  }
+
+  const handleBack=  () => {
+      navigate('/')
   }
 
   return (
     <div className={styles.container}>
       <header>
-        <img src={backimg} alt="" />
+        <img onClick={handleBack} src={backimg} alt="" />
         <Note username={userName} usercolor={userColor} shortname={shortname}></Note>
       </header>
       <main>
